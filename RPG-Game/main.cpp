af92ef3d7e5eb172aed51ebb15d13ff28b06d41d
Arjuna Bimantara <arjunabimantara91@gmail.com>
2024-01-28 19:31:52 +0700

    Added Enemy
    - added a dummy
    - added std::vector
    - preparing for shooting bullet to enemy

--- RPG-Game/main.cpp
+++ RPG-Game/main.cpp
@@ -1,22 +1,59 @@
-#include<SFML/Graphics.hpp>
+#include <SFML/Graphics.hpp>
 #include <iostream>
+#include <vector>
 
 int main()
 {
+	std::vector<std::string> topTenAnime;
+	topTenAnime.reserve(100);
+
+	for (size_t i = 0; i < 100; i++)
+	{
+		topTenAnime.push_back("Boku no hero academia");
+	}
+	
+	std::cout << "count: " << topTenAnime.size() << std::endl;
+	std::cout << "capacity: " << topTenAnime.capacity() << std::endl;
+
 	//---------------------INITIALIZE-------------------------
 	sf::ContextSettings settings;
 	settings.antialiasingLevel = 8;
-	sf::RenderWindow window(sf::VideoMode(800, 600), "Arjuna", sf::Style::Default,settings);
+	sf::RenderWindow window(sf::VideoMode(1920, 1080), "Arjuna", sf::Style::Default, settings);
 
 	//---------------------INITIALIZE-------------------------
 
 	//------------------------LOAD----------------------------
+	//--- Dummy ---
+	sf::Texture dummyTexture;
+	sf::Sprite dummySprite;
+
+	if (dummyTexture.loadFromFile("Asset/Dummy/Texture/DummySheet.png"))
+	{
+		std::cout << "Dummy texture has been loaded !" << std::endl;
+		dummySprite.setTexture(dummyTexture);
+
+		int xIndex = 0;
+		int yIndex = 0;
+
+		int xValue = xIndex * 64;
+		int yValue = yIndex * 64;
+
+		dummySprite.setTextureRect(sf::IntRect(xValue, yValue, 64, 64));
+		dummySprite.setScale(3, 3);
+	}
+	else
+	{
+		std::cout << "Dummy taxture failed to loaded!" << std::endl;
+	}
+	//--- Dummy ---
+
+	//--- Player ---
 	sf::Texture playerTexture;
 	sf::Sprite playerSprite;
 
 	if (playerTexture.loadFromFile("Asset/Player/Texture/PlayerSheet.png"))
 	{
-		std::cout << "Texture has been loaded!" << std::endl;
+		std::cout << "Player texture has been loaded!" << std::endl;
 		playerSprite.setTexture(playerTexture);
 
 		//---Manipulasi Index---
@@ -32,19 +69,21 @@ int main()
 
 		//Manipulasi ukuran sprite
 		playerSprite.setScale(sf::Vector2f(3, 3));
+		playerSprite.setPosition(sf::Vector2f(960, 540));
 	}
 	else
 	{
-		std::cout << "Texture is not loadded!" << std::endl;
+		std::cout << "Player texture failed to loadded!" << std::endl;
 	}
+	//--- Player ---
 
 	//------------------------LOAD----------------------------
-	
+
 	//GameLoop
 	while (window.isOpen())
 	{
-	//-----------------------UPDATE---------------------------
-		//----EVENT LOOP----
+		//-----------------------UPDATE---------------------------
+			//----EVENT LOOP----
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
@@ -79,9 +118,10 @@ int main()
 
 	//------------------------DRAW----------------------------
 		window.clear(sf::Color::Black);
+		window.draw(dummySprite);
 		window.draw(playerSprite);
 		window.display();
-	//------------------------DRAW----------------------------
+		//------------------------DRAW----------------------------
 	}
 
 	return 0;
